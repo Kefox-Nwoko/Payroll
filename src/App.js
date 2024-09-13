89a9c5edfa7d7cbf2ad3bf869ffe2c06f75c84a6
Kefox-Nwoko <kefox.nwoko@gmail.com>
2024-09-13 20:20:33 +0100

    "Deleted App.js and index.js files, updated node_modules dependencies, and modified package.json and yarn.lock files."

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Routes from '../pages/routes';
+import { start } from 'repl';
+
+const App = () => {
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+};
+
+export default App;
